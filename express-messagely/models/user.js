48e51daf31097870a0d3ad0b22bbb8dc47000105
Grant <grantmsmith30@gmail.com>
2023-06-28 08:54:53 -0400

    Message.ly_exercise

---
+++ express-messagely/models/user.js
@@ -0,0 +1,118 @@
+/** User class for message.ly */
+const db = require("../db");
+const ExpressError = require("../expressError");
+const bcrypt = require("bcrypt");
+const { BCRYPT_WORK_FACTOR } = require("../config");
+
+/** User of the site. */
+
+class User {
+
+  /** register new user -- returns
+   *    {username, password, first_name, last_name, phone}
+   */
+
+  static async register({username, password, first_name, last_name, phone}) {
+    let hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR); 
+    const result = await db.query('INSERT INTO users (username, password, first_name, last_name, phone, join_at, last_login_at) VALUES ($1, $2, $3, $4, $5, current_timestamp, current_timestamp) RETURNING username, password, first_name, last_name, phone', [username, hashedPassword, first_name, last_name, phone]);
+    return result.rows[0];
+   }
+
+  /** Authenticate: is this username/password valid? Returns boolean. */
+
+  static async authenticate(username, password) {
+    const result = await db.query('SELECT password FROM users WHERE username = $1', [username])
+    let user = result.rows[0];
+
+    return user && bcrypt.compare(password, user.password);
+  }
+
+  /** Update last_login_at for user */
+
+  static async updateLoginTimestamp(username) {
+    const result = await db.query('UPDATE users SET last_login_at = current_timestamp WHERE username = $1 RETURNING username', [username]);
+    if (!result.rows[0]){
+      throw new ExpressError(`No such user: ${username}`, 404);
+    }
+  }
+
+  /** All: basic info on all users:
+   * [{username, first_name, last_name, phone}, ...] */
+
+  static async all() {
+    const result = await db.query('SELECT username, first_name, last_name, phone FROM users')
+    let users = result.rows;
+    return users
+  }
+
+  /** Get: get user by username
+   *
+   * returns {username,
+   *          first_name,
+   *          last_name,
+   *          phone,
+   *          join_at,
+   *          last_login_at } */
+
+  static async get(username) {
+    const result = await db.query('SELECT username, first_name, last_name, phone, join_at, last_login_at FROM users WHERE username = $1', [username]);
+    let user = result.rows[0];
+    if(!user){
+      throw new ExpressError(`User: ${username} cannot be found`, 404)
+    }
+    return user 
+  }
+
+  /** Return messages from this user.
+   *
+   * [{id, to_user, body, sent_at, read_at}]
+   *
+   * where to_user is
+   *   {username, first_name, last_name, phone}
+   */
+
+  static async messagesFrom(username) {
+    const result = await db.query('SELECT msg.id, msg.to_username, usr.first_name, usr.last_name, usr.phone, msg.body, msg.sent_at, msg.read_at FROM messages AS msg JOIN users AS usr ON msg.to_username = usr.username WHERE from_username = $1', [username]);
+
+    return result.rows.map(m => ({
+      id: m.id,
+      to_user: {
+        username: m.to_username,
+        first_name: m.first_name,
+        last_name: m.last_name,
+        phone: m.phone
+      },
+      body: m.body,
+      sent_at: m.sent_at,
+      read_at: m.read_at
+    }));
+  }
+
+  /** Return messages to this user.
+   *
+   * [{id, from_user, body, sent_at, read_at}]
+   *
+   * where from_user is
+   *   {username, first_name, last_name, phone}
+   */
+
+  static async messagesTo(username) {
+    const result = await db.query('SELECT msg.id, msg.from_username, usr.first_name, usr.last_name, usr.phone, msg.body, msg.sent_at, msg.read_at FROM messages AS msg JOIN users AS usr ON from_username = usr.username WHERE to_username = $1', [username]);
+
+    return result.rows.map(m => ({
+      id: m.id,
+      from_user: {
+        username: m.from_username,
+        first_name: m.first_name,
+        last_name: m.last_name,
+        phone: m.phone
+      },
+      body: m.body,
+      sent_at: m.sent_at,
+      read_at: m.read_at
+    }));
+  }
+}
+
+
+module.exports = User;
